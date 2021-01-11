cf6080679e9503f2d70d191f4aa7d49ae109cb9a
yoshiya-03 <yoshiya03y07@gmail.com>
2021-01-11 17:46:24 +0900

    [Start]

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-$(document).ready(function () {  // // jQueryプログラムの内容
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function () {
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px'
+  });
 });
